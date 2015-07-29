03039172e6b9010e21c2dab0b63c7806e7c75cb2
muscle <yanshengjia1995@163.com>
2015-07-29 15:38:37 +0800

    年份

---
+++ 习题1-7.cpp
@@ -0,0 +1,23 @@
+//习题1-7
+//年份
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int year;
+    cin>>year;
+    
+    if(year%4==0)
+    {
+        if(year%100==0)
+        {
+            if(year%400==0)    cout<<"yes"<<endl;
+            else cout<<"no"<<endl;
+        }
+        else cout<<"yes"<<endl;
+    }
+    else cout<<"no"<<endl;
+    
+    return 0;
+}
