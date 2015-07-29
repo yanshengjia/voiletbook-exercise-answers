9a0280f9142ed55cf2c40dd6b9bb72fdba15114c
muscle <yanshengjia1995@163.com>
2015-07-29 15:07:08 +0800

    正弦和余弦

--- 习题1-4.cpp
+++ 习题1-4.cpp
@@ -9,7 +9,7 @@ int main()
 {
     int n;  //0<n<360
     cin>>n;
-    cout<<sin(n*M_PI/180)<<endl;    //M_PI不是真正的π，只是一个近似值，浮点数运算有误差
+    cout<<sin(n*M_PI/180)<<"  ";    //M_PI不是真正的π，只是一个近似值，浮点数运算有误差
     cout<<cos(n*M_PI/180)<<endl;
     return 0;
 }
